f0d5719c8848659bfbfa8af0a6b0b99b7280938f
Antoine James <antoine@mushow.uk>
2024-03-28 17:43:52 +0100

    Add Security Configuration

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.nnk.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests( authorize -> {
+                    authorize.requestMatchers("/user/**", "/admin/**", "/secure/**").hasAuthority("ADMIN");
+                    authorize.requestMatchers("/bidList/**", "/rating/**", "/ruleName/**", "/trade/**", "/curvePoint/**", "/app/logout").hasAnyAuthority("ADMIN", "USER");
+                    authorize.requestMatchers("/", "/error").permitAll();
+                    authorize.anyRequest().authenticated();
+                })
+                .formLogin(formLogin -> formLogin.permitAll()
+                        .defaultSuccessUrl("/bidList/list"))
+                .logout(logout -> logout.logoutUrl("/app-logout").logoutSuccessUrl("/").permitAll())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
