57dbce5cfd0e6cf387b4e379a90d58091141ff4e
Antoine James <antoine@mushow.uk>
2024-03-28 17:46:40 +0100

    Changed Trade Entity

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
@@ -1,12 +1,81 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.sql.Timestamp;
 
-
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "trade")
 public class Trade {
-    // TODO: Map columns in data table TRADE with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "trade_id")
+    private Integer tradeId;
+
+    @NotBlank(message = "Account is mandatory")
+    private String account;
+
+    @NotBlank(message = "Type is mandatory")
+    private String type;
+
+    @NotNull(message = "Buy Quantity is mandatory")
+    @PositiveOrZero(message = "Buy quantity must be positive.")
+    @Column(name = "buy_quantity")
+    private Double buyQuantity;
+
+    @Column(name = "sell_quantity")
+    private Double sellQuantity;
+
+    @Column(name = "buy_price")
+    private Double buyPrice;
+
+    @Column(name = "sell_price")
+    private Double sellPrice;
+
+    private String benchmark;
+
+    @Column(name = "trade_date")
+    private Timestamp tradeDate;
+
+    private String security;
+
+    private String status;
+
+    private String trader;
+
+    private String book;
+
+    @Column(name = "creation_name")
+    private String creationName;
+
+    @Column(name = "creation_date")
+    private Timestamp creationDate;
+
+    @Column(name = "revision_name")
+    private String revisionName;
+
+    @Column(name = "revision_date")
+    private Timestamp revisionDate;
+
+    @Column(name = "deal_name")
+    private String dealName;
+
+    @Column(name = "deal_type")
+    private String dealType;
+
+    @Column(name = "source_list_id")
+    private String sourceListId;
+
+    private String side;
+
 }
