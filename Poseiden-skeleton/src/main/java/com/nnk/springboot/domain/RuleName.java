a2a078d6430c2701265cdf03532084e0e6997d5e
Antoine James <antoine@mushow.uk>
2024-03-28 17:46:28 +0100

    Changed RuleName Entity

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -1,11 +1,42 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import java.sql.Timestamp;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "rulename")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "rule_name")
 public class RuleName {
-    // TODO: Map columns in data table RULENAME with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Description is mandatory")
+    private String description;
+
+    @NotBlank(message = "Json is mandatory")
+    private String json;
+
+    @NotBlank(message = "Template is mandatory")
+    private String template;
+
+    @NotBlank(message = "SQL string is mandatory")
+    @Column(name = "sql_str")
+    private String sqlStr;
+
+    @NotBlank(message = "SQL part is mandatory")
+    @Column(name = "sql_part")
+    private String sqlPart;
+
 }
