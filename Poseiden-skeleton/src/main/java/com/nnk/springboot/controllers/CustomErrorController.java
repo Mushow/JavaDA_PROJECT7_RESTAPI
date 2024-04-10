54d0ceec1c84978706f11639b65c81853c018434
Antoine James <antoine@mushow.uk>
2024-04-10 10:03:19 +0200

    Renamed Controller to implement ErrorController

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CustomErrorController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CustomErrorController.java
@@ -1,16 +1,17 @@
 package com.nnk.springboot.controllers;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ErrorController {
+public class CustomErrorController implements ErrorController {
 
     @GetMapping("/error")
     public String error(Model model) {
         model.addAttribute("errorMsg", "Forbidden access!");
-        return "error";
+        return "403";
     }
 
 }
